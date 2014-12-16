4158f6432918192864c2147a048a2769da1b73c9
Davis <Davis.Du@sneakerhead.com>
2014-12-16 10:37:22 +0800

    add

--- skin/frontend/default/spalding/js/iwd/opc/checkout.js
+++ skin/frontend/default/spalding/js/iwd/opc/checkout.js
@@ -433,7 +433,8 @@ IWD.OPC.Checkout = {
 			
 			
 			if (this.config.isLoggedIn===1){
-				var addressId = $j('#billing-address-select').val();
+//				var addressId = $j('#billing-address-select').val();//old opc
+				var addressId =$j("input[name='billing_address_id'][checked]").val();
 				if (addressId!='' && addressId!=undefined ){
 					IWD.OPC.Billing.save();
 				}else{
@@ -842,7 +843,7 @@ IWD.OPC.Billing = {
 					else
 						IWD.OPC.Checkout.lockPlaceOrder(1);
 					
-					IWD.OPC.Billing.bill_need_update = false;		
+					IWD.OPC.Billing.bill_need_update = false;
 					IWD.OPC.Checkout.xhr = $j.post(IWD.OPC.Checkout.config.baseUrl + 'onepage/json/saveBilling',form, IWD.OPC.Checkout.prepareAddressResponse,'json');
 			}, 500);
 		}
