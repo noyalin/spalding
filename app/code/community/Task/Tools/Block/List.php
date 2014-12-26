48f0d57e34ed8ce5410da7915c36e63243c79312
Davis <Davis.Du@sneakerhead.com>
2014-12-26 16:16:04 +0800

    add

--- app/code/community/Task/Tools/Block/List.php
+++ app/code/community/Task/Tools/Block/List.php
@@ -4,4 +4,17 @@ class Task_Tools_Block_List extends Mage_Core_Block_Template
   public function getAllStore(){
 
   }
+
+    public function getAllProvince(){
+        $return = array();
+        $resource = Mage::getSingleton('core/resource');
+        $writeConnection = $resource->getConnection('core_write');
+        $readConnection = $resource->getConnection('core_read');
+        $query = "SELECT distinct province FROM `store` ";
+        $res = $readConnection->query($query);
+        foreach($res as $each){
+            $return[] = $each['province'];
+        }
+        return  $return;
+    }
 }
