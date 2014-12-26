141203df18fd0854f3b606b4a960ee810cd4a22d
Davis <Davis.Du@sneakerhead.com>
2014-12-26 16:05:13 +0800

    add

--- app/code/community/Task/Tools/Block/List.php
+++ app/code/community/Task/Tools/Block/List.php
@@ -1,10 +1,7 @@
 <?php
 class Task_Tools_Block_List extends Mage_Core_Block_Template
 {
-    protected function _construct()
-    {
-        parent::_construct();
-        $this->setTemplate('tools/list.phtml');
-    }
+  public function getAllStore(){
 
+  }
 }
