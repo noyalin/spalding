5d8a19fb5fffe12d6d16dbd8847a8994cf51bfe3
Davis <Davis.Du@sneakerhead.com>
2015-01-15 14:18:04 +0800

    remove

--- app/code/local/Infinitech/Weixinpay/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Infinitech_Weixinpay_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
--- app/code/local/Infinitech/Weixinpay/Model/Notifypub.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 通用通知接口
- */
-class Infinitech_Weixinpay_Model_Notifypub extends CosmoCommerce_Weixinpay_Model_Wxpayserverpub
-{
-
-}
