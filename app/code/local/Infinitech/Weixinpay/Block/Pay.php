0852b65b605697cc2ca5662d9b77fb5ca6300535
Davis <Davis.Du@sneakerhead.com>
2015-02-26 15:10:31 +0800

    add

---
+++ app/code/local/Infinitech/Weixinpay/Block/Pay.php
@@ -0,0 +1,10 @@
+<?php
+class Infinitech_Weixinpay_Block_Pay extends Mage_Payment_Block_Form
+{
+    protected function _construct()
+    {
+        parent::_construct();
+//        $this->setTemplate('pay/form/pay.phtml');
+    }
+
+}
---
+++ app/code/local/Infinitech/Weixinpay/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Infinitech_Weixinpay_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
