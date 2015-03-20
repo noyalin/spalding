27f1e96e7546197871917dcd10b674a343bb73bd
Davis <Davis.Du@sneakerhead.com>
2015-03-20 17:28:39 +0800

    add

--- app/code/community/Task/Tools/controllers/IndexController.php
+++ app/code/community/Task/Tools/controllers/IndexController.php
@@ -306,11 +306,13 @@ class Task_Tools_IndexController extends Mage_Core_Controller_Front_Action{
                     Mage::log($ex->getMessage());
                 }
                 Mage::getSingleton('customer/session')->loginById($customer->getId());
-                $this->_redirect('customer/account');
+               // $this->_redirect('customer/account');
+                $this->_loginPostRedirect();
                 return;
             }else{
                 Mage::getSingleton('customer/session')->loginById($customer->getId());
-                $this->_redirect('customer/account');
+               // $this->_redirect('customer/account');
+                $this->_loginPostRedirect();
                 return;
             }
 
@@ -357,6 +359,46 @@ class Task_Tools_IndexController extends Mage_Core_Controller_Front_Action{
         }
         curl_close($ci);
         return $response;
+    }
+
+    protected function _loginPostRedirect()
+    {
+        $session = $this->_getSession();
+
+        if (!$session->getBeforeAuthUrl() || $session->getBeforeAuthUrl() == Mage::getBaseUrl()) {
+            // Set default URL to redirect customer to
+            $session->setBeforeAuthUrl($this->_getHelper('customer')->getAccountUrl());
+            // Redirect customer to the last page visited after logging in
+            if ($session->isLoggedIn()) {
+                if (!Mage::getStoreConfigFlag(
+                    Mage_Customer_Helper_Data::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD
+                )) {
+                    $referer = $this->getRequest()->getParam(Mage_Customer_Helper_Data::REFERER_QUERY_PARAM_NAME);
+                    if ($referer) {
+                        // Rebuild referer URL to handle the case when SID was changed
+                        $referer = $this->_getModel('core/url')
+                            ->getRebuiltUrl( $this->_getHelper('core')->urlDecode($referer));
+                        if ($this->_isUrlInternal($referer)) {
+                            $session->setBeforeAuthUrl($referer);
+                        }
+                    }
+                } else if ($session->getAfterAuthUrl()) {
+                    $session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
+                }
+            } else {
+                $session->setBeforeAuthUrl( $this->_getHelper('customer')->getLoginUrl());
+            }
+        } else if ($session->getBeforeAuthUrl() ==  $this->_getHelper('customer')->getLogoutUrl()) {
+            $session->setBeforeAuthUrl( $this->_getHelper('customer')->getDashboardUrl());
+        } else {
+            if (!$session->getAfterAuthUrl()) {
+                $session->setAfterAuthUrl($session->getBeforeAuthUrl());
+            }
+            if ($session->isLoggedIn()) {
+                $session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
+            }
+        }
+        $this->_redirectUrl($session->getBeforeAuthUrl(true));
     }
 
 }
