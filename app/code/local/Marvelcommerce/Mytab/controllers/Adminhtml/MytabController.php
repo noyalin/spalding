20fc7a7fbc847475feccf392ec251c4fc96dc92d
Davis <Davis.Du@sneakerhead.com>
2015-04-24 15:06:53 +0800

    add

--- app/code/local/Marvelcommerce/Mytab/controllers/Adminhtml/MytabController.php
+++ app/code/local/Marvelcommerce/Mytab/controllers/Adminhtml/MytabController.php
@@ -4,7 +4,7 @@ class Marvelcommerce_Mytab_Adminhtml_MytabController extends Mage_Adminhtml_Cont
 
     public function indexAction()
     {
-
+//        echo "aaa";
         $this->loadLayout();
         $this->renderLayout();
     }
