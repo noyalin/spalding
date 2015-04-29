10e979c726be41c5840a2eccdab8506baf346cf2
Davis <Davis.Du@sneakerhead.com>
2015-04-29 16:21:21 +0800

    add

--- app/code/community/Task/Tools/controllers/CheckController.php
+++ app/code/community/Task/Tools/controllers/CheckController.php
@@ -1,16 +1,11 @@
 <?php
 class Task_Tools_CheckController extends Mage_Core_Controller_Front_Action{
 
-    public function indexAction(){
-        echo 'a';
-    }
-
     public function emailAction(){
         if (!$this->getRequest()->isPost()) {
             return;
         }
         $email = $this->getRequest()->getParam('email');
-        mage :: log($email);
         $websiteId = Mage::app()->getWebsite()->getId();
 
         echo $this->IscustomerEmailExists($email);
@@ -21,7 +16,7 @@ class Task_Tools_CheckController extends Mage_Core_Controller_Front_Action{
         $customer->setWebsiteId(1);
         $customer->loadByEmail($email);
         if ($customer->getId()) {
-            return $customer->getId();
+            return 1;
         }
         return false;
     }
