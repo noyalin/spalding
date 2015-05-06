573a9fdc75d96907d51a5d0b0b9ad9b68f8619b7
Davis <Davis.Du@sneakerhead.com>
2015-05-06 12:16:08 +0800

    add

--- skin/frontend/default/spalding/js/register.js
+++ skin/frontend/default/spalding/js/register.js
@@ -2,8 +2,12 @@ jQuery(function(){
     jQuery("#agree_terms").click(function(){
         if("checked" == jQuery(this).attr("checked")){
             jQuery("#advice-required-entry-agree_terms").hide();
+            jQuery("#submitbutton").show();
+            jQuery("#disablesubmitbutton").hide();
         }else{
             jQuery("#advice-required-entry-agree_terms").show();
+            jQuery("#submitbutton").hide();
+            jQuery("#disablesubmitbutton").show();
         }
 
     });
