15beea7f408b2acbba92370965aefaa7d793851c
davis <davis.du@sneakerhead.com>
2015-06-05 15:10:49 -0700

    add sql for weixin dev module

--- app/code/local/Devicom/Weixindev/controllers/IndexController.php
+++ app/code/local/Devicom/Weixindev/controllers/IndexController.php
@@ -2,6 +2,6 @@
 class Devicom_Weixindev_IndexController extends Mage_Core_Controller_Front_Action{
 
     public function indexAction(){
-        
+
     }
 }
---
+++ app/code/local/Devicom/Weixindev/sql/weixindev_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,44 @@
+<?php
+$installer = $this;
+
+$installer->startSetup();
+
+Mage::log("Running installer weixinpay");
+$installer->run("
+
+DROP TABLE IF EXISTS `{$this->getTable('weixin_user')}` ;
+CREATE TABLE `{$this->getTable('weixin_user')}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(500) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `createtime` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `TrackingID` varchar(50) NOT NULL,
+  `sessionlast` varchar(100) DEFAULT NULL,
+  `session_identity_num` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `openid` (`openid`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1  ;
+
+DROP TABLE IF EXISTS `{$this->getTable('weixin_identity')}` ;
+CREATE TABLE `{$this->getTable('weixin_identity')}` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `identity_number` char(18) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB   DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `{$this->getTable('weixin_token')}` ;
+CREATE TABLE `{$this->getTable('weixin_token')}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(500) CHARACTER SET utf8 NOT NULL,
+  `createtime` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB   DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+");
+
+$installer->endSetup();
+
