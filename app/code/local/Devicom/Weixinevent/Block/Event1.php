53f54bbf439996db6dff72a9982bd51cf0a17d62
Gubuchun <gubuchun@voyageone.cn>
2015-06-15 19:46:30 +0800

    weixin

--- app/code/local/Devicom/Weixinevent/Block/Event1.php
+++ app/code/local/Devicom/Weixinevent/Block/Event1.php
@@ -16,4 +16,9 @@ class Devicom_Weixinevent_Block_Event1 extends Mage_Payment_Block_Form
         $url = "http://localhost/spalding/weixinevent/index/index/oid/".$orderId."/aid/".$actId;
         QRCode_Code::createCode($url);
     }
+
+    public function getVar()
+    {
+        return Mage::getSingleton('weixinevent/promotion')->getVar();
+    }
 }
