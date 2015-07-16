2c19684778597bf043e91fe27349e62f248bfa11
Bob.chen <bob.chen@voyageone.cn>
2015-07-16 17:39:38 +0800

    微信bug
    Signed-off-by: Bob.chen <bob.chen@voyageone.cn>

--- app/code/local/Devicom/Weixindev/Model/Wxbase.php
+++ app/code/local/Devicom/Weixindev/Model/Wxbase.php
@@ -604,7 +604,7 @@ EOF;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查，0表示阻止对证书的合法性的检查。
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $r = curl_exec($ch);
 
