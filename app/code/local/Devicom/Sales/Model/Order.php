72abb2dd534615460b8e0809146dea785d2a89df
Bob.chen <bob.chen@voyageone.cn>
2015-08-14 14:15:45 +0800

    cancel order finla
    Signed-off-by: Bob.chen <bob.chen@voyageone.cn>

--- app/code/local/Devicom/Sales/Model/Order.php
+++ app/code/local/Devicom/Sales/Model/Order.php
@@ -3,15 +3,15 @@
 class Devicom_Sales_Model_Order extends Mage_Sales_Model_Order
 {
 
-    /**
-     * Cancel order
-     *
-     * @return Devicom_Sales_Model_Order
-     */
-    public function cancel()
-    {
-        $aaa = 'OK';
-        return $this;
-    }
+//    /**
+//     * Cancel order
+//     *
+//     * @return Devicom_Sales_Model_Order
+//     */
+//    public function cancel()
+//    {
+//        $aaa = 'OK';
+//        return $this;
+//    }
 
 }
