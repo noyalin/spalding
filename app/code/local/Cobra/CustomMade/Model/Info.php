c56e17ef55a2c70d88762aba17f242c0fb2a96ef
Gubuchun <buchun.gu@voyageone.cn>
2015-08-26 18:19:46 +0800

    3D定制球
    Signed-off-by: Gubuchun <buchun.gu@voyageone.cn>

--- app/code/local/Cobra/CustomMade/Model/Info.php
+++ app/code/local/Cobra/CustomMade/Model/Info.php
@@ -59,17 +59,81 @@ class Cobra_CustomMade_Model_Info extends Mage_Core_Model_Abstract
                         ->setMsg2P1($session->getContent2P1())
                         ->setMsg3P1($session->getContent3P1())
                         ->setMsg4P1($session->getContent4P1())
+                        ->setMsg5P1(self::getP1Url($session,'show'))
+                        ->setMsg6P1(self::getP1Url($session,'print'))
                         ->setTypeP2($session->getTypeP2())
                         ->setMsg1P2($session->getContent1P2())
                         ->setMsg2P2($session->getContent2P2())
                         ->setMsg3P2($session->getContent3P2())
                         ->setMsg4P2($session->getContent4P2())
+                        ->setMsg5P2(self::getP1Url($session,'show'))
+                        ->setMsg6P2(self::getP1Url($session,'print'))
                         ->setStatus(self::STATUS_NON_PAYMENT)
                         ->save();
                     $this->clearSession($customerId, $sku);
                 }
             }
         }
+    }
+
+    private function getP1Url($session, $type)
+    {
+        $url = 'http://s7d5.scene7.com/is/image/sneakerhead/spalding-';
+        $textone = $session->getContent1P1();
+        $texttwo = $session->getContent2P1();
+        $size = $session->getContent3P1();
+//        $font = $session->getContent4P1();
+//        switch($font){
+//            case 0:
+//                break;
+//            default:
+//        }
+        switch ($size) {
+            case 1:
+                $url .= 'p1-small_one-' . $type . '?$1980pxx544px$&$textone=' . $textone;
+                break;
+            case 2:
+                $url .= 'p1-middle-' . $type . '?$1980pxx544px$&$text=' . $textone;
+                break;
+            case 3:
+                $url .= 'p1-big-' . $type . '?$1980pxx544px$&$text=' . $textone;
+                break;
+            case 4:
+            default:
+                $url .= 'p1-small_two-' . $type . '?$1980pxx544px$&$texttwo=' . $texttwo . '&$textone=' . $textone;
+        }
+
+        return $url;
+    }
+
+    private function getP2Url($session, $type)
+    {
+        $url = 'http://s7d5.scene7.com/is/image/sneakerhead/spalding-';
+        $textone = $session->getContent1P2();
+        $texttwo = $session->getContent2P2();
+        $size = $session->getContent3P2();
+//        $font = $session->getContent4P2();
+//        switch($font){
+//            case 0:
+//                break;
+//            default:
+//        }
+        switch ($size) {
+            case 1:
+                $url .= 'p2-small_one-' . $type . '?$1980pxx544px$&$textone=' . $textone;
+                break;
+            case 2:
+                $url .= 'p2-middle-' . $type . '?$1980pxx544px$&$text=' . $textone;
+                break;
+            case 3:
+                $url .= 'p2-big-' . $type . '?$1980pxx544px$&$text=' . $textone;
+                break;
+            case 4:
+            default:
+                $url .= 'p2-small_two-' . $type . '?$1980pxx544px$&$texttwo=' . $texttwo . '&$textone=' . $textone;
+        }
+
+        return $url;
     }
 
     private function clearSession($customerId, $sku)
