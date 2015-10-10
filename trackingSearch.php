2fe575655690ce3ec26c3cc336e08bf01cfbc8c5
carrie <carrie.zhang@voyageone.cn>
2015-10-10 11:50:51 +0800

    custom made
    Signed-off-by: carrie <carrie.zhang@voyageone.cn>

--- trackingSearch.php
+++ trackingSearch.php
@@ -13,14 +13,24 @@ curl_close($ch);
 $xml = simplexml_load_string($r);
 $return = "";
 if ($xml == false) {
-    $return = "抱歉！";
+    $return = '<p>抱歉！未查到此运单<span class="checkError">32316027456537</span>信息，请<span class="checkError">确认运单号码</span>是否正确，订单号码相关信息请咨询155-4622-9782，更多物流动态可咨询95338。</p>';
 } else {
     $xmlArray = (array)$xml;
     $items = array_reverse($xmlArray['row']);
+    $i = 0;
+    $return .= '<ul class="confirmSn">';
     foreach ($items as $item) {
         $itemArray = (array)$item;
+        if ($i == 0) {
+            $return .= '<li class="conNow">';
+        } else {
+            $return .= '<li>';
+        }
         $return .= '<p>' . $itemArray['trackevent'] . '</p>';
-        $return .= '<p>' . $itemArray['trackdatetime'] . '</p><br />';
-    }
+        $return .= '<p>' . $itemArray['trackdatetime'] . '</p>';
+        $return .= '</li>';
+        $i++;
+    } 
+    $return .= '</ul>';
 }
 echo $return;
