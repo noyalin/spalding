a46e556afa4ee8abce1fe08decdb70029c4d9de8
Gubuchun <buchun.gu@voyageone.cn>
2015-11-04 18:20:14 +0800

    mail
    Signed-off-by: Gubuchun <buchun.gu@voyageone.cn>

--- app/code/community/Task/Tools/Model/CustomMadeMail.php
+++ app/code/community/Task/Tools/Model/CustomMadeMail.php
@@ -35,9 +35,9 @@ class Task_Tools_Model_CustomMadeMail extends Task_Tools_Model_Base
 
     public function sendNotification($this_subject, $this_message, $cc = false, $subject_override = false)
     {
-        $to = self :: CONF_SYSTEM_NOTIFICATION_CC_ADDRESS;
-        $to .= ($cc) ? "," . 'pisces.bian@voyageone.cn' : "";
-        $subject = ($subject_override) ? $this_subject : "System Notification - $this_subject";
+        $to = 'pisces.bian@voyageone.cn';
+        $to .= ($cc) ? "," . 'kobe.xin@voyageone.cn,aaron.deng@voyageone.cn,bob.chen@voyageone.cn,buchun.gu@voyageone.cn' : "";
+        $subject = ($subject_override) ? $this_subject : "Spalding System Notification - $this_subject";
         $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
 
         $message = "This is a system notification.\r\n\r\n";
