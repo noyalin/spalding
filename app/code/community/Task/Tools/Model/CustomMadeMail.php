edfa4a5c7cd0d9835f0900cf0aa579c4dffe77ce
Gubuchun <buchun.gu@voyageone.cn>
2015-11-05 11:26:40 +0800

    mail
    Signed-off-by: Gubuchun <buchun.gu@voyageone.cn>

--- app/code/community/Task/Tools/Model/CustomMadeMail.php
+++ app/code/community/Task/Tools/Model/CustomMadeMail.php
@@ -4,8 +4,8 @@ class Task_Tools_Model_CustomMadeMail extends Task_Tools_Model_Base
 {
     public function execute()
     {
-        $subject = '待审批订单';
-        $message = "昨日尚未审批订单号：\r\n";
+        $subject = '斯伯丁官网定制球待审批订单';
+        $message = date("Y年m月d日", time() - 24 * 60 * 60)."尚未审批订单号：\r\n";
         $orderIds = Mage::getModel('custommade/info')->loadByTime(date("Y-m-d", time()) . ' 00:00:00', date("Y-m-d", time() - 24 * 60 * 60) . ' 00:00:00', 1);
         if (empty($orderIds)) {
             $message .= '无' . "\r\n";
@@ -24,8 +24,8 @@ class Task_Tools_Model_CustomMadeMail extends Task_Tools_Model_Base
                 $message .= $orderId['order_id'] . "\r\n";
             }
         }
-
-        $this->sendNotification($subject, $message, true);
+        $message .= "审批链接：http://www.spaldingchina.com.cn/index.php/backendspalding\r\n";
+        $this->sendNotification($subject, $message, true, true);
     }
 
     public function validate()
@@ -35,8 +35,9 @@ class Task_Tools_Model_CustomMadeMail extends Task_Tools_Model_Base
 
     public function sendNotification($this_subject, $this_message, $cc = false, $subject_override = false)
     {
-        $to = 'pisces.bian@voyageone.cn';
-        $to .= ($cc) ? "," . 'kobe.xin@voyageone.cn,aaron.deng@voyageone.cn,bob.chen@voyageone.cn,buchun.gu@voyageone.cn' : "";
+//        $to = 'pisces.bian@voyageone.cn';
+//        $to .= ($cc) ? "," . 'kobe.xin@voyageone.cn,aaron.deng@voyageone.cn,bob.chen@voyageone.cn,buchun.gu@voyageone.cn' : "";
+        $to = 'buchun.gu@voyageone.cn';
         $subject = ($subject_override) ? $this_subject : "Spalding System Notification - $this_subject";
         $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
 
