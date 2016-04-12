ef31777094a2e355fff08595111b5ef38b6fa25b
Gubuchun <buchun.gu@voyageone.cn>
2016-04-12 19:07:03 +0800

    支付bug

--- skin/frontend/base/default/js/alipay.js
+++ skin/frontend/base/default/js/alipay.js
@@ -12,11 +12,12 @@ function setBank(url, bank, order_id) {
 function setLocation(url){
     window.location.href = url;
 }
- 
+
 function go_pay() {
     jQuery("body").append("<div id='overlay' style='background:#000;display:block;z-index:300;width:1200px;position:absolute;top:0;left:0;'></div>"), jQuery(document).scrollTop();
     var c = jQuery(document).scrollLeft(),
-        d = jQuery(window).height() ,h = jQuery(document).height() ,//jQuery(document).height(),
+        d = jQuery(window).height() ,
+        h = jQuery(document).height() ,
         e = jQuery(window).width();
     jQuery("#overlay").css({
         opacity: "0.2",
@@ -24,53 +25,13 @@ function go_pay() {
         left: c,
         width: e
     });
-    // if(bank){
-    //     // window.open(a+"?bank="+bank);
-    //     var link = jQuery("#pay");
-    //     link.attr('href',a+"?bank="+bank);
-    //     // link.triggerHandler("click");
-    // }else{
-    //     // window.open(a);
-    //     var link = jQuery("#pay");
-    //     link.attr('href',a);
-    //     // link.triggerHandler("click");
-    // }
-    //console.log(jQuery('.col-main').offset().left);
-    //console.log(e);
-    //console.log(jQuery('#go_pay_window').outerWidth());
     var left  = (e/2)-(jQuery('#go_pay_window').outerWidth()/2),
     top   = (jQuery(window).height()/2)-(160/2);
     jQuery('#go_pay_window').css('left',left+"px");
     jQuery('#go_pay_window').css('top',top+"px");
     jQuery('#go_pay_window').fadeIn();
 }
-function go_pay_with_weixin(a,bank,order_id) {
-    jQuery("body").append("<div id='overlay' style='background:#000;display:block;z-index:300;width:1200px;position:absolute;top:0;left:0;'></div>"), jQuery(document).scrollTop();
-    var c = jQuery(document).scrollLeft(),
-        d = jQuery(window).height() ,h = jQuery(document).height() ,//jQuery(document).height(),
-        e = jQuery(window).width();
-    jQuery("#overlay").css({
-        opacity: "0.2",
-        height: h,
-        left: c,
-        width: e
-    });
-    // if(bank){
-    //     // window.open(a+"?bank="+bank+"&orderid="+order_id);
-    //     jQuery("#pay").attr('action',a+"?bank="+bank+"&orderid="+order_id).submit();
-    // }else{
-    //     // window.open(a);
-    //     jQuery("#pay").attr('action',a).submit();
-    // }
-    //console.log(jQuery('.col-main').offset().left);
-    //console.log(e);
-    //console.log(jQuery('#go_pay_window').outerWidth());
-    var left  = (e/2)-(jQuery('#go_pay_window').outerWidth()/2),
-        top   = (jQuery(window).height()/2)-(160/2);
-    jQuery('#go_pay_window').css('left',left+"px");
-    jQuery('#go_pay_window').css('top',top+"px");
-    jQuery('#go_pay_window').fadeIn();
-}
+
 function window_close() {
     jQuery("#overlay").length > 0 && jQuery("#overlay").remove(), jQuery(".popup-wrap.popup-orderEnd").fadeOut()
 }
