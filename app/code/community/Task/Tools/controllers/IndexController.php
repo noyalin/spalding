9be3551bd267667b92af0f20add0a1dcda3b9e40
bob.chen <bob.chen@voyageone.cn>
2016-04-29 01:12:05 +0800

    删除测试代码

--- app/code/community/Task/Tools/controllers/IndexController.php
+++ app/code/community/Task/Tools/controllers/IndexController.php
@@ -130,22 +130,22 @@ class Task_Tools_IndexController extends Mage_Core_Controller_Front_Action{
     public function indexAction(){
 //        $this->loadLayout();
 //        $this->renderLayout();
-        $str = file_get_contents("php://input");
-        if(!$str){
-            $str = '{"tid":"580415280761034","name":"东北人19680624","price":"2349.0"}';
-        }
-
-        $arr = json_decode($str,true);
-        $price = $arr['price'];
-
-
-        $content = '<img width=800 src="'.'http://s7d5.scene7.com/is/image/sneakerhead/800x318?$800x318$&$amount='.$price.'" alt="" border="0" />';
-        $content .= '<div id="homeNewarrivalTitle">
-                    <h2>NEW ARRIVALS</h2>
-                    <div class="clearfloats">&nbsp;</div>
-                    </div>';
-        Mage::getModel('cms/block')->load('tmall_1111_high')->setData('content',$content)->save();
-        echo "success";
+//        $str = file_get_contents("php://input");
+//        if(!$str){
+//            $str = '{"tid":"580415280761034","name":"东北人19680624","price":"2349.0"}';
+//        }
+//
+//        $arr = json_decode($str,true);
+//        $price = $arr['price'];
+//
+//
+//        $content = '<img width=800 src="'.'http://s7d5.scene7.com/is/image/sneakerhead/800x318?$800x318$&$amount='.$price.'" alt="" border="0" />';
+//        $content .= '<div id="homeNewarrivalTitle">
+//                    <h2>NEW ARRIVALS</h2>
+//                    <div class="clearfloats">&nbsp;</div>
+//                    </div>';
+//        Mage::getModel('cms/block')->load('tmall_1111_high')->setData('content',$content)->save();
+//        echo "success";
 //        $layout = Mage :: getSingleton('core/layout');
 //        $block = $layout->createBlock("cms/block");
 //        $block->setBlockId('tmall_1111_high');
