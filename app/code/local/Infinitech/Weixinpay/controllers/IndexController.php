650b052a147c2ba2ed93ead8064397566377a1c3
bob.chen <bob.chen@voyageone.cn>
2016-09-13 14:25:28 +0800

    增加跳转到h5的接口

--- app/code/local/Infinitech/Weixinpay/controllers/IndexController.php
+++ app/code/local/Infinitech/Weixinpay/controllers/IndexController.php
@@ -7,19 +7,27 @@ class Infinitech_Weixinpay_IndexController extends Mage_Core_Controller_Front_Ac
 //        $this->renderLayout();
 //        $weixinpay = Mage::getModel('weixinpay/pay');
 //        echo $weixinpay->getAppId();
-        echo Infinitech_Weixinpay_Model_Wxpaypubconfig :: getCode('app_id');
+        // echo Infinitech_Weixinpay_Model_Wxpaypubconfig :: getCode('app_id');
     }
 
     public function testAction(){
-        echo "test";
-//        $wxOrderQuery = Mage::getModel('weixinpay/Orderquerypub');
-//        $ret = $wxOrderQuery->orderQuery('100007182_1444990829');
-//
-//        if ($ret == false) {
-//            echo "订单查询失败";
-//        } else {
-//            echo "订单查询成功 ".$ret;
-//        }
+        $url = 'http://www.baidu.com/';
+        $queryStr = $_SERVER['QUERY_STRING'];
+        if($queryStr != ''){
+            $url.= "?".$queryStr;
+        }
+        $this->_redirectUrl($url);
+        return;
 
+    }
+
+    public function h5Action(){
+        $url = 'http://h5.spaldingchina.com.cn/auth.php';
+        $queryStr = $_SERVER['QUERY_STRING'];
+        if($queryStr != ''){
+            $url.= "?".$queryStr;
+        }
+        $this->_redirectUrl($url);
+        return;
     }
 }
