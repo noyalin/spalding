fe2bc2bec3a09269b8fbc7e00937c445cdc30896
Terry Yao <terry.yao@voyageone.cn>
2016-09-13 14:18:59 +0800

    Order remove update

--- app/code/community/Task/Tools/Model/OrderOverCancel.php
+++ app/code/community/Task/Tools/Model/OrderOverCancel.php
@@ -6,6 +6,8 @@ class Task_Tools_Model_OrderOverCancel extends Task_Tools_Model_Base{
     }
     public function execute(){
     	
+    	$resource = Mage::getSingleton('core/resource');
+    	$readConnection = $resource->getConnection('core_read');
     	$overTime = date('Y-m-d H:i:s',strtotime("-24 hours"));
     	$orders = Mage::getModel('sales/order')->getCollection()
     	->addFieldToFilter('state', 'new')
@@ -13,15 +15,28 @@ class Task_Tools_Model_OrderOverCancel extends Task_Tools_Model_Base{
     	->addFieldToFilter('created_at', array('lt'=>$overTime));
     	if(count($orders) > 0){
     		foreach ($orders as $order){
-//    			$id = $order->getIncrementId();
-//     			if($id == '100000004'){
-//     				$order->setState('canceled');
-//     				$order->setStatus('canceled');
-//     				$order->save();
-    				//exit;
-//     			}else{
-    				//break;
-//     			}
+    			$orderId = $order->getIncrementId();
+    			$orderModel = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+    			$orderItems = $order->getItemsCollection();
+    			if(count($orderItems) > 0){
+    				foreach ($orderItems as $item){
+    					$productId = $item->product_id;
+    					$productSku = $item->sku;
+    					$product = Mage::getModel('catalog/product')->load($productId);
+    					$type = $product->getTypeId();
+    					if($type == 'simple'){
+    						$stock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
+    						$sql = "SELECT `qty` FROM `devicom_physical_inventory` WHERE `sku` = '$productSku'";
+    						$results = intval($readConnection->fetchOne($sql));
+    						if($results > 0){
+    							$stock->setQty($results);
+    						}else{
+    							$stock->setQty(0);
+    						}
+    						$stock->save();
+    					}
+    				}
+    			}
     			$order->setState('canceled');
     			$order->setStatus('canceled');
     			$order->save();
@@ -29,12 +44,9 @@ class Task_Tools_Model_OrderOverCancel extends Task_Tools_Model_Base{
     	}
     	echo "update over";
     }        
-
-   
+    
     public function validate(){
         return false;
     }
-
-   
 
 }
